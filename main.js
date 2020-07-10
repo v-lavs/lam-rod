ef997b64334dc1106e3aabb189dfbe01025bc7af
v-lavs <lavskavanda@gmail.com>
2020-07-10 23:11:10 +0300

    added slider

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ jQuery(document).ready(function ($) {
 
                 $('html, body').animate({
                     scrollTop: $($.attr(this, 'href')).offset().top - 50
-                }, 2000);
+                }, 150);
             }
         });
     }
@@ -67,7 +67,7 @@ jQuery(document).ready(function ($) {
     var $galleryStrip = $('.gallery-list');
 
     function runScroll() {
-        if ($(window).width() >  940) {
+        if ($(window).width() > 940) {
             var contentHeight = $contentWrap.height();
             var boundingGalleryWrap = $galleryWrap.getBoundingClientRect();
             var boundingGalleryTop = boundingGalleryWrap.top;
@@ -82,8 +82,7 @@ jQuery(document).ready(function ($) {
             }
 
             $galleryStrip.css({transform: 'translate3d(0,' + -offsetY + 'px , 0)'});
-        }
-        else {
+        } else {
             $galleryStrip.css({transform: 'translate3d(0,0,0)'});
         }
     }
@@ -99,6 +98,7 @@ jQuery(document).ready(function ($) {
     function LightBox(id, params = {}) {
         // params
         var animationTime = params.duration || 450;
+        var isSlider = params.isSlider || false;
         var infoSelector = params.infoSelector;
 
         // nodes
@@ -112,6 +112,7 @@ jQuery(document).ready(function ($) {
         var contentToClone = $(infoSelector);
         var currentIndex = 0;
         var withInfo = false;
+        var slider;
 
         //methods
         function goNext(e) {
@@ -134,15 +135,41 @@ jQuery(document).ready(function ($) {
         function insertSlideContent(node) {
             var url = $(node).attr('href');
             var isIframe = $(node).attr('data-embedded');
-
+            var mediaContent;
+            console.log(withInfo)
             if (withInfo) {
                 $details.html($(node).find(infoSelector).html());
             }
 
-            if (!isIframe && url) {
-                $contentSlide.html('<img class="img-responsive" src="' + url + '"/>');
+            if (isSlider) {
+                var innerSlides = JSON.parse($(node).attr('data-slides') || '[]');
+                var $sliderWrap = $('<div class="swiper-container" id="ls-slider"><div class="swiper-wrapper"></div><div class="swiper-arrow swiper-button-next"></div><div class="swiper-arrow swiper-button-prev"></div></div>');
+                var $sliderContent = $sliderWrap.find(".swiper-wrapper");
+
+                innerSlides.forEach(function (item) {
+                    $sliderContent.append('<div class="swiper-slide"><img class="img-responsive" src="' + item + '"/></div>');
+                });
+                $contentSlide.html($sliderWrap.get(0));
+                slider = new Swiper('#ls-slider', {
+                    loop: true,
+                    navigation: {
+                        nextEl: '.swiper-arrow.swiper-button-next',
+                        prevEl: '.swiper-arrow.swiper-button-prev',
+                    }
+                });
+
+            } else if (!isIframe && url) {
+                mediaContent = $('<img class="img-responsive" src="' + url + '"/>')
             } else if (isIframe && url) {
-                $contentSlide.html('<iframe class="ls-iframe" width="560" height="315" src="' + url + '" frameborder="0" allow="accelerometer autoplay encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+                mediaContent = $('<iframe class="ls-iframe" width="560" height="315" src="' + url + '" frameborder="0" allow="accelerometer autoplay encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+            }
+            if (!isSlider) {
+                var fadeAnim = 420;
+                $contentSlide.fadeOut(fadeAnim);
+                setTimeout(function () {
+                    $contentSlide.html(mediaContent);
+                }, fadeAnim);
+                $contentSlide.fadeIn(fadeAnim);
             }
         }
 
@@ -151,6 +178,9 @@ jQuery(document).ready(function ($) {
             $lightbox.fadeOut(animationTime);
 
             setTimeout(function () {
+                if (slider) {
+                    slider.destroy(true, true);
+                }
                 $contentSlide.html('');
                 $details.html('');
             }, animationTime);
@@ -166,6 +196,7 @@ jQuery(document).ready(function ($) {
         }
 
         function showDescription() {
+            console.log(contentToClone.length);
             if (contentToClone.length > 0) {
                 withInfo = true;
                 $lightbox.addClass('withInfo');
@@ -195,15 +226,17 @@ jQuery(document).ready(function ($) {
     LightBox('gallery');
 
     LightBox('gallery-projects-progress', {
-        infoSelector: '.project-info'
+        infoSelector: '.project-info',
+        isSlider: true
     });
 
     LightBox('projects-gallery', {
-        infoSelector: '.project-info'
+        infoSelector: '.project-info',
+        isSlider: true
     });
 
-      // ANIMATION
-      var fadeInBlocks = $('.fade-in').waypoint(function (direction) {
+    // ANIMATION
+    var fadeInBlocks = $('.fade-in').waypoint(function (direction) {
         $(this.element).addClass('active')
     }, {
         offset: '75%'
